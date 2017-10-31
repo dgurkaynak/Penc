455f2e8d8323ddd08d7b8c21b614cd2b07970f57
Deniz Gurkaynak <dgurkaynak@gmail.com>
2017-10-31 13:44:29 +0300

    Initial Commit

---
+++ Penc/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Penc
+//
+//  Created by Deniz Gurkaynak on 31.10.2017.
+//  Copyright © 2017 Deniz Gurkaynak. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Penc/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Penc
+//
+//  Created by Deniz Gurkaynak on 31.10.2017.
+//  Copyright © 2017 Deniz Gurkaynak. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
