9f4a5290111c5c586ef4932e4282453db71c69ec
Deniz Gurkaynak <dgurkaynak@gmail.com>
2021-01-17 13:06:26 +0300

    refactor(PlaceholderWindow): set window styles in controller instead of storyboard

--- Penc/PlaceholderWindowViewController.swift
+++ Penc/PlaceholderWindowViewController.swift
@@ -14,7 +14,13 @@ class PlaceholderWindowViewController: NSViewController, NSWindowDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do view setup here.
+        self.box.borderType = .grooveBorder
+        self.box.borderWidth = 3
+        self.box.cornerRadius = 5
+        self.box.borderColor = NSColor(calibratedRed: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        self.box.fillColor = NSColor(calibratedRed: 0, green: 0, blue: 0, alpha: 0.25)
     }
     
     func updateWindowSizeTextField(_ windowFrame: CGRect) {
